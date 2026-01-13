5744e9134dc4a4b97d3e065b15dfe6c92bf3562c
Gus <intel4004u@gmail.com>
2026-01-13 18:02:30 +0000

    Code getHumanChoice and start playRound function

--- rps.js
+++ rps.js
@@ -1,4 +1,14 @@
-console.log("Hello JS World!!git")
+const myListBox = document.querySelector("#rps_selection");
+const myButton = document.querySelector("#send");
+const myPara = document.querySelector(".myPara");
+const humanScoreBoard = document.querySelector(".humanScoreBoard");
+const computerScoreBoard = document.querySelector(".computerScoreBoard");
+let rounds = 1;
+let computerScore = 0;
+let humanScore = 0;
+
+myButton.addEventListener("click", playRound)
+
 
 function getComputerChoice() {
     const rndNum = Math.floor(Math.random() * 3) + 1
@@ -8,6 +18,23 @@ function getComputerChoice() {
         return 'Paper'
     else
         return 'Scissors'
+    console.log(`Computer choice is: ${myListBox.value}`)
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    return myListBox.value
+}
+
+function playRound() {
+    const humanChoice = getHumanChoice()
+    const computerChoice = getComputerChoice()
+    console.log(`Human: ${humanChoice}`)
+    console.log(`Computer: ${computerChoice}`)
+
+    if (humanChoice == computerChoice){
+        myPara.textContent = "It's a draw";
+    } else if (humanChoice == 'Rock' && computerChoice == 'Paper'){
+        
+    }
+
+}
