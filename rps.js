60ff04f8931f21481e64aa919ed77dbf675ea58d
Gus <intel4004u@gmail.com>
2026-01-13 16:48:09 +0000

    Code getComputerChoice so it returns rock, paper or scissors

--- rps.js
+++ rps.js
@@ -1,1 +1,13 @@
 console.log("Hello JS World!!git")
+
+function getComputerChoice() {
+    const rndNum = Math.floor(Math.random() * 3) + 1
+    if (rndNum == 1)
+        return 'Rock'
+    else if (rndNum == 2)
+        return 'Paper'
+    else
+        return 'Scissors'
+}
+
+getComputerChoice()
