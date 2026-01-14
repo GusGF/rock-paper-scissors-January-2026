93c143bf117b6237b472baa69458e8be0817d889
Gus <intel4004u@gmail.com>
2026-01-14 17:56:06 +0000

    Implement playing one round at a time

--- rps.js
+++ rps.js
@@ -1,6 +1,6 @@
 const myListBox = document.querySelector("#rps_selection");
 const myButton = document.querySelector("#send");
-const myPara = document.querySelector(".myPara");
+const playerMsg = document.querySelector(".playerMsg");
 const humanScoreBoard = document.querySelector(".humanScoreBoard");
 const computerScoreBoard = document.querySelector(".computerScoreBoard");
 let rounds = 1;
@@ -32,9 +32,29 @@ function playRound() {
     console.log(`Computer: ${computerChoice}`)
 
     if (humanChoice == computerChoice){
-        myPara.textContent = "It's a draw";
+        playerMsg.textContent = "It's a draw";
     } else if (humanChoice == 'Rock' && computerChoice == 'Paper'){
-        
+        playerMsg.textContent = "Well Done Computer, paper beats rock"
+        computerScore++;
+    }  else if (humanChoice == 'Rock' && computerChoice == 'Scissors'){
+        playerMsg.textContent = "Well Done Human, rock beats scissors"
+        humanScore++;
+    }  else if (humanChoice == 'Paper' && computerChoice == 'Rock'){
+        playerMsg.textContent = "Well Done Human, paper beats rock"
+        humanScore++;
+    }  else if (humanChoice == 'Paper' && computerChoice == 'Scissors'){
+        playerMsg.textContent = "Well Done Computer, scissors beats paper"
+        computerScore++;
+    }   else if (humanChoice == 'Scissors' && computerChoice == 'Rock'){
+        playerMsg.textContent = "Well Done Computer, rock beats scissors"
+        computerScore++;
+    }  else if (humanChoice == 'Scissors' && computerChoice == 'Paper'){
+        playerMsg.textContent = "Well Done Human, scissors beats paper"
+        humanScore++;
+    } else {
+        playerMsg.textContent = "No idea what went wrong"
     }
 
+    humanScoreBoard.textContent = humanScore;
+    computerScoreBoard.textContent = computerScore;
 }
